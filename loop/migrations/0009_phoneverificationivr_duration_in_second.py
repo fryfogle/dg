17d6d302460336bef65f04382d7e9a3914791c66
sainivikas <vikassaini480@gmail.com>
2017-03-02 10:38:59 +0530

    migration added for loop

---
+++ loop/migrations/0009_phoneverificationivr_duration_in_second.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('loop', '0008_phoneverificationivr'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='phoneverificationivr',
+            name='duration_in_second',
+            field=models.IntegerField(default=-1),
+        ),
+    ]
